6833ae6a761e6d83ff7d3db0c77d0fc91f5e89a9
AlekseyKorshuk <ale-kor02@mail.ru>
2022-03-19 13:19:58 +0300

    fix: pipeline

--- onnx_transformers/pipelines.py
+++ onnx_transformers/pipelines.py
@@ -712,7 +712,7 @@ class Pipeline(_ScikitCompat):
         # TODO: add option gpt2 if need
         print(f"Creating quantized graph from {self.graph_path.as_posix()} to {quantized_model_path.as_posix()}")
         opt_options = FusionOptions('bert')
-        opt_options.enable_embed_layer_norm = False
+        # opt_options.enable_embed_layer_norm = False
         opt_options.enable_gelu_approximation = True
         onnx_opt_model = optimizer.optimize_model(self.graph_path.as_posix(),
                                                   'bert_tf' if self.framework == "tf" else "bert",
